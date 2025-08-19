283406cbdd177543539a286aabfffe6b60b34a95
LuiVLoureiro <luivloureiro@gmail.com>
2025-08-19 19:05:40 -0300

    ftr: Criação dos Dados Aleatorios

--- 19-08-2025/data/create_data.py
+++ 19-08-2025/data/create_data.py
@@ -7,6 +7,7 @@ class Criar():
     def __init__(self):
         pass
 
+
     def arquivo_csv(self, nome=""):
         try:
             dir_atual = os.path.dirname(os.path.abspath(__file__))
@@ -22,24 +23,42 @@ class Criar():
             print(f"error: Erro na Criação do Arquivo {nome}.csv")
 
     def dados_no_csv(self):
-        # ts (1 Mês)
+
+        nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo", "Fernanda", "Gabriel", "Helena", "Igor", "Juliana"]
+        jogos = ["Reflexo", "Memória"]
+
+        # ts (Intervalo de 1 Mes)
         inicio = np.datetime64("2025-01-01T00:00:00")
         fim    = np.datetime64("2025-01-31T23:59:59")
         rng = np.random.default_rng(42)
         span = int((fim - inicio) / np.timedelta64(1, "s"))
-        offs = rng.integers(0, span, size=10)
+        offs = rng.integers(0, span, size=31)
         ts = inicio + offs.astype("timedelta64[s]")
+        iso = np.datetime_as_string(ts, unit="s")
 
-        print(ts)
+        for data in iso:
+            # 
+            nome = np.random(nomes)
+            jogo = np.random(jogos)
 
-        # player
-        nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo", "Fernanda", "Gabriel", "Helena", "Igor", "Juliana"]
+            arquivo = os.path.dirname(os.path.abspath(__file__))
+            df = pd.DataFrame(
+                'ts': [data],
+                'player': [],
+                'game': [],
+                'score': []
+                )
 
-        # game
-        jogos = ["Dado", "Reflexo", "Memória"]
 
-        # score 
-        scores = np.random.randint(64)
+    def jogo_reflexo(self):
+        pares_acertados = np.random.randint(0, 10)
+        score = pares_acertados * 10
+        return score
+
+    def jogo_memoria(self):
+        tempo_total_ms = np.random.randint(0, 500)
+        score = 500 - (tempo_total_ms/10)
+        return score
 
 
 bot = Criar()
