25aea699216c298b4c0138c0240224fc107cdc4a
LuiVLoureiro <luivloureiro@gmail.com>
2025-08-19 17:26:11 -0300

    ftr: Adicionar Aleatorizacao dos Dadospara as Datas

--- 19-08-2025/data/create_data.py
+++ 19-08-2025/data/create_data.py
@@ -20,10 +20,28 @@ class Criar():
             print(f"sucess: Criação do {nome}.csv foi feita com sucesso")
         except:
             print(f"error: Erro na Criação do Arquivo {nome}.csv")
+
     def dados_no_csv(self):
-        pass
+        # ts (1 Mês)
+        inicio = np.datetime64("2025-01-01T00:00:00")
+        fim    = np.datetime64("2025-01-31T23:59:59")
+        rng = np.random.default_rng(42)
+        span = int((fim - inicio) / np.timedelta64(1, "s"))
+        offs = rng.integers(0, span, size=10)
+        ts = inicio + offs.astype("timedelta64[s]")
+
+        print(ts)
+
+        # player
+        nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo", "Fernanda", "Gabriel", "Helena", "Igor", "Juliana"]
+
+        # game
+        jogos = ["Dado", "Reflexo", "Memória"]
+
+        # score 
+        scores = np.random.randint(64)
 
 
 bot = Criar()
 
-bot.arquivo_csv("arcade")
+bot.dados_no_csv()
