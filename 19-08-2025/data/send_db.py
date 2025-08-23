8c2e7400f7e57597e0f9df81fb9c204587bddd0b
LuiVLoureiro <luivloureiro@gmail.com>
2025-08-23 15:02:33 -0300

    ftr: Adicionar Tabelas e Classes a Database

--- 19-08-2025/data/send_db.py
+++ 19-08-2025/data/send_db.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, DateTime, ForeignKey, Float, Text
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 import os
 
 caminho = os.path.dirname(os.path.abspath(__file__))
@@ -10,5 +10,31 @@ Sessao = sessionmaker(bind=db)
 sessao = Sessao()
 
 Base = declarative_base()
+
+# Classes
+class Partida(Base):
+    __tablename__ = "partida"
+    id_partida = Column("id_partida", Integer, primary_key=True, autoincrement=True)
+    datetime = Column("ts", DateTime)
+    id_jogador = Column("id_jogador", Integer, ForeignKey("jogadores.id_jogador"), nullable=False)
+    id_jogo = Column("id_jogo", Integer, ForeignKey("jogos.id_jogo"), nullable=False)
+    score = Column("score", Float)
+
+    jogador = relationship("Jogadores", back_populates="partida")
+    jogo = relationship("Jogos", back_populates="partida")
+
+class Jogadores(Base):
+    __tablename__ = "jogadores"
+    id_jogador = Column("id_jogador", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", Text)
+
+    partida = relationship("Partida", back_populates="jogadores")
+
+class Jogos(Base):
+    __tablename__ = "jogos"
+    id_jogo = Column("id_jogo", Integer, primary_key=True, autoincrement=True)
+    nome = Column("nome", Text)
+
+    partida = relationship("Partida", back_populates="jogos")
 
 Base.metadata.create_all(bind=db)
